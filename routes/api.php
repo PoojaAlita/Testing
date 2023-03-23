46be94beee68465091a5845b392dd2d82b664aa7
shamsabbas <shams@alitainfotech.com>
2023-03-23 10:34:58 +0530

    initial commit

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\{AuthController, ForgotPasswordController};
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
+//Route Of Login
+Route::post('login', [AuthController::class, 'login']);
+Route::post('registration', [AuthController::class, 'registration']);
+//Route Of Forgot Password
+Route::get('/reset-password/{token}', [ForgotPasswordController::class, 'resetPassword']);
+Route::post('forgot-password', [ForgotPasswordController::class, 'forgotPassword']);
+Route::get('unAuthnticated', [AuthController::class, 'unAuthnticated'])->name('unAuthnticated');
+//Route Of Reset Password
+Route::group(['middleware' => 'auth:api'], function () {
+    //Route Of User Add
+    Route::post('/store', [AuthController::class, 'store']);
+    //Route Of User Edit
+    Route::put('/edit/{id}', [AuthController::class, 'edit']);
+    //Route Of User Listing
+    Route::post('/listing', [AuthController::class, 'listing']);
+    //Route Of User Delete
+    Route::delete('/delete/{id}', [AuthController::class, 'delete']);
+    //Route Of User Logout
+    Route::post('logout', [AuthController::class, 'logout']);
+    //Route Of User Profile
+    Route::get('/profile-update/{id}', [AuthController::class, 'profile_update']);
+    //Route Of Change password
+    Route::get('/change-password/{id}', [ForgotPasswordController::class, 'changePassword']);
 });
+
+
+
+
+/* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+}); */
